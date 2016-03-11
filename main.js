86d0060fd8c9394589e9c03e94e9872e28624860
Robin Scott <robinandrewscott@gmail.com>
2016-03-11 17:05:04 +0000

    Applied active home fix

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $(function() {
-	console.log('HEY');
-    if ((location.pathname.split("/")[1]) !== ""){
-            $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
-        }
+    if ((location.pathname !== "/"){
+    	$('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+    } else {
+    	var home = document.getElementById("home").getElementsByTagName('a')[0];
+    	home.className = 'current';
+    }
 });
