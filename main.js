bb91d3c65e8c0a75143cee21b5aaa2dccdce4a7a
Robin Scott <robinandrewscott@gmail.com>
2016-03-11 17:05:52 +0000

    Fixed js syntax error

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(function() {
-    if ((location.pathname !== "/"){
+    if (location.pathname !== "/") {
     	$('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
     } else {
     	var home = document.getElementById("home").getElementsByTagName('a')[0];
