74de283438c0f5b7232e703df7f719f42c6efd29
Robin Scott <robinandrewscott@gmail.com>
2016-03-11 17:11:43 +0000

    JS fix

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(function() {
     if (location.pathname !== "/") {
     	$('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
     } else {
-    	var home = document.getElementById("home").getElementsByTagName('a')[0];
+    	var home = document.getElementById("home-link");
     	home.className = 'current';
     }
 });
