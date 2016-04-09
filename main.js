5b38d6c7474a7a59c0974cb39bc488e0af1fe13a
Robin Scott <robinandrewscott@gmail.com>
2016-04-09 21:27:33 +0100

    Changed nav links styling

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ $(function() {
     if (location.pathname !== "/") {
     	$('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
     } else {
+    	console.log('hey');
     	var home = document.getElementById("home-link");
-    	home.className = 'current';
+    	home.className = 'nav-link active';
     }
 });
