f36a079114b1d5f120661056d23fad47bd2b4523
Robin Scott <robinandrewscott@gmail.com>
2016-04-17 20:22:18 +0100

    Added favicon

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ $(function() {
     if (location.pathname !== "/") {
     	$('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
     } else {
-    	console.log('hey');
     	var home = document.getElementById("home-link");
     	home.className = 'nav-link active';
     }
